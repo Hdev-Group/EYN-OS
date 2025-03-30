fd57c795694bf66a79046dfdcc3d85df757a4ad5
Kian Gentry <musinks@proton.me>
2025-03-30 17:17:24 +0100

    knew i was forgetting something

--- src/entry/kernel.c
+++ src/entry/kernel.c
@@ -9,6 +9,6 @@ int kmain()
 {
 	isr_install();
 	clearScreen();
-	printf_colored("EYN-OS v0.02\nType 'help' for a list of commands.\n\n", 7, 0);
+	printf_colored("EYN-OS v0.03\nType 'help' for a list of commands.\n\n", 7, 0);
   	launch_shell(0);
 }
